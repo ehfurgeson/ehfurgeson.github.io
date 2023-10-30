f0e3a317b64ec4ae3b81a56385ca8e55e3b294d9
pyeonline <34693439+pyeonline@users.noreply.github.com>
2023-10-29 20:00:44 -0400

    lots of updates and changes, this is complete for the assignment I need

---
+++ atrail/script.js
@@ -0,0 +1,22 @@
+function adjustLayout() {
+    var calc = document.getElementById("calc");
+    var map = document.getElementById("map");
+
+    if (window.innerWidth > window.innerHeight){
+        calc.style.width = "50vw";
+        calc.style.height = "90vh";
+        map.style.width = "50vw";
+        map.style.height = "90vh";
+        document.getElementById("total").style.flexDirection = "row";
+    }
+    else if (window.innerWidth < window.innerHeight){
+        calc.style.width = "100vw";
+        calc.style.height = "45vh";
+        map.style.width = "100vw";
+        map.style.height = "45vh";
+        document.getElementById("total").style.flexDirection = "column";
+    }
+}
+
+window.addEventListener('resize', adjustLayout);
+window.addEventListener('load', adjustLayout);
